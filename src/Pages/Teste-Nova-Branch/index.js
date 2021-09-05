ee80a7642f75cdf46639b99478fe286bb5a260fb
Thiago Leite <thiago_francisco09@live.com>
2021-09-05 19:53:12 -0300

    nova pagina

---
+++ src/Pages/Teste-Nova-Branch/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const index = () => {
+  return (
+    <View>
+      <Text>Testando a criação de uma nova branch</Text>
+    </View>
+  );
+};
+
+export default index;
