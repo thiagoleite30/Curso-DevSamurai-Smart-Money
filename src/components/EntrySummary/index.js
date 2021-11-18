388044c32d63e2602bb06d514bb6adec3afbc27c
Thiago Leite <thiago_francisco09@live.com>
2021-11-18 18:33:10 -0300

    Ajuste no gráfico da página de relatórios (reports) para que a filtragem pelo número de dias altere o gráfico de pizza

--- src/components/EntrySummary/index.js
+++ src/components/EntrySummary/index.js
@@ -9,14 +9,6 @@ import EntrySummaryList from './EntrySummaryList';
 
 import useBalanceSumByCategory from '../../hooks/useBalanceSumByCategory';
 
-const entriesGrouped = [
-  { key: '1', description: 'Alimentação', amount: 200 },
-  { key: '2', description: 'Combustível', amount: 12 },
-  { key: '3', description: 'Aluguel', amount: 120 },
-  { key: '4', description: 'Lazer', amount: 250 },
-  { key: '5', description: 'Outros', amount: 1200 },
-];
-
 const EntrySummary = ({ days = 7, onPressActionButton }) => {
   const [balanceSum] = useBalanceSumByCategory(days);
 
